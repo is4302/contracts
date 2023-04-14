86af1a2cde41217b1716c3e4a059b582a13cf159
Changrui <78465180+Ch40gRv1-Mu@users.noreply.github.com>
2023-04-14 21:47:02 +0800

    update readme

---
+++ MedicalRecords.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PrescriptionVerification {
+    address[] public patients; // List of patients
+    address[] public doctors; // List of doctors
+    mapping (address => bool) isDoctor;
+    mapping (address => bool) isPatient;
+    mapping (address => uint256[]) doctorRecords; // Records of prescriptions created by each doctor
+    mapping (address => uint256[]) patientRecords; // Records of prescriptions approved by each patient
+    mapping (uint256 => Prescription) private medicalRecords; // All prescription records
+    address owner;
+    uint256 nonce = 1;
+    mapping (bytes32 => uint256) private hashToNonce; // Map prescription hash to its nonce
+
+
+    // Events
+    event PatientAdded(address patient);
+    event DoctorAdded(address doctor);
+    event PrescriptionAdded(uint256 nonce, address doctor, address patient);
+    event PrescriptionApproved(uint256 nonce, address patient);
+
+    struct Prescription {
+        address doctor;
+        address patient;
+        bytes32 prescriptionHash;
+        bool approved;
+    }
+
+    modifier onlyPatient(address patientAddress) {
+        require(msg.sender == patientAddress, "Only patient can call this function");
+        _;
+    }
+
+    modifier onlyDoctor(address senderAddress) {
+        require(isDoctor[senderAddress] == true, "Doctor is not registered");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Add a new patient
+    function addPatient(address patientAddress) public {
+        require(patientAddress != address(0), "Invalid patient address");
+        require(isPatient[patientAddress] == false, "Patient was already registeated");
+        patients.push(patientAddress);
+        emit PatientAdded(patientAddress);
+    }
+
+    // Add a new doctor
+    function addDoctor(address doctorAddress) public {
+        require(doctorAddress != address(0), "Invalid doctor address");
+        require(isDoctor[doctorAddress] == false, "Doctor was already registeated");
+        require(msg.sender == owner, "Only owner can add doctors")
+        doctors.push(doctorAddress);
+        isDoctor[doctorAddress] = true;
+        emit DoctorAdded(doctorAddress);
+    }
+
+    // Add a new prescription
+    function addPrescription(address patientAddress, bytes32 prescriptionHash) public onlyDoctor(msg.sender) {
+        medicalRecords[nonce] = Prescription(msg.sender, patientAddress, prescriptionHash, false);
+        doctorRecords[msg.sender].push(nonce);
+        patientRecords[patientAddress].push(nonce);
+        hashToNonce[prescriptionHash] = nonce;
+        emit PrescriptionAdded(nonce, msg.sender, patientAddress);
+        nonce += 1;
+    }
+
+    // Approve an existing prescription
+    function approvePrescription(address patientAddress, uint256 _nonce) public onlyPatient(patientAddress) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        require(prescription.doctor != address(0), "Prescription does not exist for patient");
+        require(prescription.patient == msg.sender, "You are not the patient of this prescription");
+        require(prescription.approved == false, "This prescription has been approved already.");
+        prescription.approved = true;
+        emit PrescriptionApproved(_nonce, patientAddress);
+    }
+
+    // Get a prescription by nonce
+    function getPrescription(uint256 _nonce) public view returns (bytes32, bool) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        return (prescription.prescriptionHash, prescription.approved);
+    }
+
+    // Getter functions for medical records
+    function getPrescriptionDoctor(uint256 _nonce) public view returns (address) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        return prescription.doctor;
+    }
+
+    function getPrescriptionPatient(uint256 _nonce) public view returns (address) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        return prescription.patient;
+    }
+
+    function getPrescriptionHash(uint256 _nonce) public view returns (bytes32) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        return prescription.prescriptionHash;
+    }
+
+    function isPrescriptionApproved(uint256 _nonce) public view returns (bool) {
+        Prescription storage prescription = medicalRecords[_nonce];
+        return prescription.approved;
+    }
+
+    // Getter functions for doctor and patient records
+    function getDoctorRecord(address doctorAddress, uint256 index) public view returns (uint256) {
+        require(isDoctor[doctorAddress], "Doctor is not registered");
+        return doctorRecords[doctorAddress][index];
+    }
+
+    function getPatientRecord(address patientAddress, uint256 index) public view returns (uint256) {
+        return patientRecords[patientAddress][index];
+    }
+
+    function getDoctorRecordCount(address doctorAddress) public view returns (uint256) {
+        require(isDoctor[doctorAddress], "Doctor is not registered");
+        return doctorRecords[doctorAddress].length;
+    }
+
+    function getPatientRecordCount(address patientAddress) public view returns (uint256) {
+        return patientRecords[patientAddress].length;
+    }
+
+    // Getter functions for patients and doctors arrays
+    function getPatient(uint256 index) public view returns (address) {
+        require(index < patients.length, "Index out of range");
+        return patients[index];
+    }
+
+    function getDoctor(uint256 index) public view returns (address) {
+        require(index < doctors.length, "Index out of range");
+        return doctors[index];
+    }
+
+    function getPatientCount() public view returns (uint256) {
+        return patients.length;
+    }
+
+    function getDoctorCount() public view returns (uint256) {
+        return doctors.length;
+    }
+
+    function isDoctorRegistrated(address doctorAddress) public view returns (bool) {
+        return isDoctor[doctorAddress];
+    }
+
+    function isPatientRegistrated(address doctorAddress) public view returns (bool) {
+        return isPatient[doctorAddress];
+    }
+
+    // Get the number of patients
+    function getNumberOfPatients() public view returns (uint256) {
+        return patients.length;
+    }
+
+    // Get the number of doctors
+    function getNumberOfDoctors() public view returns (uint256) {
+        return doctors.length;
+    }
+
+    // Get the list of medical records for a doctor
+    function getDoctorMedicalRecords(address doctorAddress) public view returns (uint256[] memory) {
+        return doctorRecords[doctorAddress];
+    }
+
+    // Get the list of medical records for a patient
+    function getPatientMedicalRecords(address patientAddress) public view returns (uint256[] memory) {
+        return patientRecords[patientAddress];
+    }
+
+    // Get nonce by prescription hash
+    function getNonceByHash(bytes32 prescriptionHash) public view returns (uint256) {
+        uint256 _nonce = hashToNonce[prescriptionHash];
+        require(_nonce != 0 || medicalRecords[0].prescriptionHash == prescriptionHash, "Prescription hash not found");
+        return _nonce;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# contract
+# Prescription Verification Smart Contract
+
+This repository contains a smart contract for prescription verification in a healthcare system. The smart contract is written in Solidity and deployed on the Ethereum network. It allows doctors to create prescription records and patients to approve them.
+
+## Structure
+
+The repo is organized as follows:
+
+- `contracts/` - Contains the main smart contract file `PrescriptionVerification.sol`.
+- `migrations/` - Contains migration files for deploying the smart contract on the Ethereum network.
+- `test/` - Contains unit tests for the smart contract.
+- `LICENSE` - License information for the project.
+- `README.md` - This file, providing a brief overview of the project.
+- `truffle-config.js` - Configuration file for the Truffle framework.
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. Clone the repository:
+
+``git clone https://github.com/is4302/contracts.git``
+
+
+2. Change into the `contracts/` directory:
+
+``cd contracts``
+
+
+3. Install dependencies:
+
+``npm install``
+
+
+4. Compile the smart contract:
+
+``truffle compile``
+
+
+5. Deploy the smart contract to a local development network:
+
+``truffle develop``
+
+
+6. Run tests:
+
+``truffle test``
+
+
+## Smart Contract Overview
+
+The `PrescriptionVerification` smart contract has the following main components:
+
+- **Patients and Doctors**: Patients and doctors are represented as Ethereum addresses. The smart contract keeps track of registered patients and doctors using arrays and mapping.
+- **Prescription Records**: Prescription records are stored in a struct containing the doctor's address, patient's address, prescription hash, and approval status.
+- **Access Control**: The smart contract has various access control modifiers to ensure that only the appropriate parties can perform specific actions.
+
+The smart contract also emits events when patients are added, doctors are added, prescriptions are created, and prescriptions are approved.
+
+## Contributing
+
+Feel free to fork the repository and submit pull requests with any improvements or bug fixes. If you encounter any issues, please report them in the GitHub issue tracker.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for more information.
